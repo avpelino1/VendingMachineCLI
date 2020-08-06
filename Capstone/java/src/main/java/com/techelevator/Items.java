59764ba999bd4abaec1bf6d91a505d82984003f8
Steve Boese <steve.f.boese@gmail.com>
2020-08-06 16:48:22 -0400

    Adding maps

--- Capstone/java/src/main/java/com/techelevator/Items.java
+++ Capstone/java/src/main/java/com/techelevator/Items.java
@@ -3,9 +3,30 @@ package com.techelevator;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Items extends Connector {
+Map<String, String> itemChoice = new HashMap<String, String>() ; {
+
+	itemChoice.put("A1", "Potato Crisps");
+	itemChoice.put("A2", "Stackers");
+	itemChoice.put("A3", "Grain Waves");
+	itemChoice.put("A4", "Cloud Popcorn");
+	itemChoice.put("B1", "Moonpie");
+	itemChoice.put("B2", "Cowtales");
+	itemChoice.put("B3", "Wonka Bar");
+	itemChoice.put("B4", "Crunchie");
+	itemChoice.put("C1", "Cola");
+	itemChoice.put("C2", "Dr. Salt");
+	itemChoice.put("C3", "Mountain Melter");
+	itemChoice.put("C4", "Heavy");
+	itemChoice.put("D1", "U-Chews");
+	itemChoice.put("D2", "Little League Chew");
+	itemChoice.put("D3", "Chiclets");
+	itemChoice.put("D4", "Trimplemint");
+}
 
 	private String codeOfItem;
 	private BigDecimal priceOfItem;
