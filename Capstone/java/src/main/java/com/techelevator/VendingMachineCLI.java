633b9aa6aef751a7ca6e4985158814749c5b77b4
Alisa Pelino <avpelino1@gmail.com>
2020-08-06 11:01:28 -0400

    added method listItems

--- Capstone/java/src/main/java/com/techelevator/VendingMachineCLI.java
+++ Capstone/java/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -12,11 +12,9 @@ public class VendingMachineCLI {
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
 	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
 
-	private static final String[] PURCHASE_MENU = {"Feed Money", "Select Product", "Back"};
-	private static final String[] MONEY_MENU = {"$1 Bill", "$2 Bill", "$5 Bill", "Back"};
-	
-	
-	
+	private static final String[] PURCHASE_MENU = { "Feed Money", "Select Product", "Back" };
+	private static final String[] MONEY_MENU = { "$1 Bill", "$2 Bill", "$5 Bill", "Back" };
+
 	private Menu menu;
 
 	public VendingMachineCLI(Menu menu) {
@@ -27,55 +25,65 @@ public class VendingMachineCLI {
 		while (true) {
 			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
 			System.out.println(choice);
-			
-			
+
 			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
 				// display vending machine items
-				File inputFile = new File("vendingmachine.csv");
-				
-				try (Scanner inputScanner = new Scanner(inputFile)) {
-					
-					while (inputScanner.hasNextLine()) {
-						String food = inputScanner.nextLine();
-						String [] foodSplit = food.split("|");
-						 
-						
-							System.out.println(foodSplit.length);
-						}
-					} catch (FileNotFoundException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-			
-				
-				
+				listItems();
+
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
-				
-				
-				
+
 				processPurchaseMenuOptions();
-				
+
 			}
+		}
+	}
+
+	private void listItems() {
+		File inputFile = new File("vendingmachine.csv");
+
+		try (Scanner inputScanner = new Scanner(inputFile)) {
+
+			while (inputScanner.hasNextLine()) {
+				String food = inputScanner.nextLine();
+				String[] foodSplit = food.split("|");
+
+				if (foodSplit[0].equals("A")) {
+					int endIndex = food.length() - 10;
+					String item = food.substring(3, endIndex);
+					System.out.println(item);
+				} else if (foodSplit[0].equals("B") || foodSplit[0].equals("C")) {
+					int endIndex = food.length() - 11;
+					String item = food.substring(3, endIndex);
+					System.out.println(item);
+				} else if (foodSplit[0].equals("D")) {
+					int endIndex = food.length() - 9;
+					String item = food.substring(3, endIndex);
+					System.out.println(item);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
 	private void processPurchaseMenuOptions() {
 		String purchaseMenuOption = "";
-		
-		while (!purchaseMenuOption.equals("Back")) {
-		purchaseMenuOption =(String) menu.getChoiceFromOptions(PURCHASE_MENU);
 
-		if (purchaseMenuOption.equals("Feed Money")) {
-			processMoneyFeed();
-		}
-		
+		while (!purchaseMenuOption.equals("Back")) {
+			purchaseMenuOption = (String) menu.getChoiceFromOptions(PURCHASE_MENU);
+
+			if (purchaseMenuOption.equals("Feed Money")) {
+				processMoneyFeed();
+			}
+
 		}
 	}
 
 	private void processMoneyFeed() {
 		String feedOptions = "";
 		while (!feedOptions.equals("Back")) {
-		feedOptions = (String) menu.getChoiceFromOptions(MONEY_MENU);
+			feedOptions = (String) menu.getChoiceFromOptions(MONEY_MENU);
 		}
 	}
 
