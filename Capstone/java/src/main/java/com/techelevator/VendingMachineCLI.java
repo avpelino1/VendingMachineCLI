869e3d056d0e843e897a051a7d86bc9ec1997d6a
Steve Boese <steve.f.boese@gmail.com>
2020-08-06 10:46:47 -0400

    kicking off the capstone

--- Capstone/java/src/main/java/com/techelevator/VendingMachineCLI.java
+++ Capstone/java/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -1,5 +1,9 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import com.techelevator.view.Menu;
 
 public class VendingMachineCLI {
@@ -8,6 +12,11 @@ public class VendingMachineCLI {
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
 	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
 
+	private static final String[] PURCHASE_MENU = {"Feed Money", "Select Product", "Back"};
+	private static final String[] MONEY_MENU = {"$1 Bill", "$2 Bill", "$5 Bill", "Back"};
+	
+	
+	
 	private Menu menu;
 
 	public VendingMachineCLI(Menu menu) {
@@ -17,12 +26,56 @@ public class VendingMachineCLI {
 	public void run() {
 		while (true) {
 			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
-
+			System.out.println(choice);
+			
+			
 			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
 				// display vending machine items
+				File inputFile = new File("vendingmachine.csv");
+				
+				try (Scanner inputScanner = new Scanner(inputFile)) {
+					
+					while (inputScanner.hasNextLine()) {
+						String food = inputScanner.nextLine();
+						String [] foodSplit = food.split("|");
+						 
+						
+							System.out.println(foodSplit.length);
+						}
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+			
+				
+				
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
-				// do purchase
+				
+				
+				
+				processPurchaseMenuOptions();
+				
 			}
+		}
+	}
+
+	private void processPurchaseMenuOptions() {
+		String purchaseMenuOption = "";
+		
+		while (!purchaseMenuOption.equals("Back")) {
+		purchaseMenuOption =(String) menu.getChoiceFromOptions(PURCHASE_MENU);
+
+		if (purchaseMenuOption.equals("Feed Money")) {
+			processMoneyFeed();
+		}
+		
+		}
+	}
+
+	private void processMoneyFeed() {
+		String feedOptions = "";
+		while (!feedOptions.equals("Back")) {
+		feedOptions = (String) menu.getChoiceFromOptions(MONEY_MENU);
 		}
 	}
 
