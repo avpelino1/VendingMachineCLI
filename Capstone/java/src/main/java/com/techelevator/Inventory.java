8d6e8fef87fbcfee7be95e5c705f4ce284be2a1c
Steve Boese <steve.f.boese@gmail.com>
2020-08-07 14:44:29 -0400

    saving

--- Capstone/java/src/main/java/com/techelevator/Inventory.java
+++ Capstone/java/src/main/java/com/techelevator/Inventory.java
@@ -1,5 +1,37 @@
 package com.techelevator;
 
-public class Inventory {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
 
+public class Inventory {
+public Map<String, List<Product>> vendingInventory = new TreeMap<>();
+
+	public List<Items> masterList = new ArrayList<Items>();
+
+	File inputFile = new File("vendingmachine.csv");
+	{
+		try (Scanner inventoryScanner = new Scanner(inputFile)) {
+			while (inventoryScanner.hasNextLine()) {
+				String inventory = inventoryScanner.nextLine();
+				String[] inventoryString = inventory.split("\\|");
+				
+				if (inventoryString[0].contains("A")) {
+					for (int i =0; i <= 4; i++) {
+						Chip newChip = new Chip(inventoryString[1], inventoryString[2], inventoryString[3]);
+						masterList.add(newChip);
+						
+					}
+				}
+				
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
