d6634d6e7c1009d80d7438d2aeddb4767b19be1e
Alisa Pelino <avpelino1@gmail.com>
2020-08-08 19:38:28 -0400

    updated test class MoneyHandlerTests

--- Capstone/java/src/test/java/com/techelevator/MoneyHandlerTests.java
+++ Capstone/java/src/test/java/com/techelevator/MoneyHandlerTests.java
@@ -1,13 +1,16 @@
 package com.techelevator;
 
+import java.io.FileNotFoundException;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 public class MoneyHandlerTests {
 
-	BigDecimal start = new BigDecimal(5.35);
+	BigDecimal start = new BigDecimal(2);
 	MoneyHandler moneyHandler = new MoneyHandler(start);
 	
 	@Test
@@ -17,13 +20,25 @@ public class MoneyHandlerTests {
 		Assert.assertEquals(expected, actual);
 	}
 
-	BigDecimal test1 = new BigDecimal(3);
+	BigDecimal test1 = new BigDecimal(1.15);
+	BigDecimal test2 = new BigDecimal(3.15);
 	@Test
-	public void moneyInputTest() {
-		moneyHandler.moneyInput(test1);
-//		BigDecimal actual = moneyHandler.currentBalance;
-		BigDecimal expected = start;
-//		Assert.assertEquals(expected, actual);
+	public void moneyInputTest() throws FileNotFoundException {
+		moneyHandler.moneyInput(test2);
+		
+		BigDecimal actual = moneyHandler.getCurrentBalance();
+		BigDecimal expected = test1;
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void changeGivenTest() {
+		List<Integer> actual = moneyHandler.changeGiven();
+		List<Integer> expected = new ArrayList<Integer>();
+		expected.add(12);
+		expected.add(1);
+		expected.add(1);
+		Assert.assertEquals(expected, actual);
 	}
 	
 }
